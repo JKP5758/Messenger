99ccf07e29440256d37820cea9954fd786dd8c9f
JKP5758 <jokopurnomo22012006@gmail.com>
2024-04-05 13:17:12 +0700

    Konvigurasi koneksi dan .env

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,13 @@
 <?php
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass =  "";
-$dbname = "db_messenger";
+// Baca isi file .env
+$envVars = parse_ini_file('.env');
+
+// Setel variabel-variabel dari file .env
+$dbhost = $envVars['DB_HOST'];
+$dbuser = $envVars['DB_USER'];
+$dbpass = $envVars['DB_PASS'];
+$dbname = $envVars['DB_NAME'];
+
 
 $koneksi = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
