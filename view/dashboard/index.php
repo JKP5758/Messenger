604a95d951f965297afa660607d3e4464334acb3
JKP5758 <jokopurnomo22012006@gmail.com>
2024-04-07 01:18:38 +0700

    Penambahan code SQL pada dashboard

--- view/dashboard/index.php
+++ view/dashboard/index.php
@@ -9,7 +9,7 @@
     $id = $_SESSION["id"];
 
     $query = mysqli_query($koneksi, "SELECT username, nama, profil FROM user WHERE id = '$id'");
-    $row = mysqli_fetch_assoc($query);
+    $row = mysqli_fetch_assoc($query)
 ?>
 
 <!DOCTYPE html>
@@ -24,35 +24,45 @@
 <body>
     <?php
         require "../template/navbar.php";
-    ?>
 
-    <div class="container">
-        <a href="../chat">
-            <div class="people">
-                <img class="foto_profil" src="../../aset/profil/01.png" alt="Foto Profil">
-                <div class="nama_pesan">
-                    <!-- <span class="nama">joko Purnomo</span> -->
-                    <input type="text" value="Joko Purnomo" class="nama" id="" readonly>
-                    <input type="text" value="lorem adhiusgdwajhdn oaiwhd anp awd aowijda" class="pesan" id="" readonly>
-                </div>
-                <div class="waktu">
-                    <input type="text" value="19.00" class="jam" id="" readonly>
-                </div>
+        $query_relasi = mysqli_query($koneksi, "SELECT * FROM `relasi` WHERE id_user1 = '$id' OR id_user2 = '$id';");
+        
+
+        while ($hasil_relasi = mysqli_fetch_assoc($query_relasi)) {
+
+            if ($id == $hasil_relasi['id_user1'] ) {
+                $partner = $hasil_relasi['id_user2'];
+            } else {
+                $partner = $hasil_relasi['id_user1'];
+            }
+
+            $query_data_partner = mysqli_query($koneksi, "SELECT nama, profil FROM `user` WHERE id = '$partner'");
+            $hasil_data_partner = mysqli_fetch_assoc($query_data_partner);
+
+            $token = $hasil_relasi['token'];
+            $query_pesan = mysqli_query($koneksi, "SELECT * FROM `data_pesan` WHERE token = '$token' ORDER BY tanggal DESC LIMIT 1");
+            $pesan = mysqli_fetch_assoc($query_pesan);
+
+            $jam = substr($pesan['jam'], 0, 5);
+    ?>
+            <div class="container">
+                <a href="../chat">
+                    <div class="people">
+                        <img class="foto_profil" src="../../aset/profil/<?=$hasil_data_partner['profil']?>" alt="Foto Profil">
+                        <div class="nama_pesan">
+                            <!-- <span class="nama">joko Purnomo</span> -->
+                            <input type="text" value="<?=$hasil_data_partner['nama']?>" class="nama" id="" readonly>
+                            <input type="text" value="<?=$pesan['pesan']?>" class="pesan" id="" readonly>
+                        </div>
+                        <div class="waktu">
+                            <input type="text" value="<?=$jam?>" class="jam" id="" readonly>
+                        </div>
+                    </div>
+                </a>
             </div>
-        </a>
-        <a href="">
-            <div class="people">
-                <img class="foto_profil" src="../../aset/profil/01.png" alt="Foto Profil">
-                <div class="nama_pesan">
-                    <!-- <span class="nama">joko Purnomo</span> -->
-                    <input type="text" value="Joko Purnomo" class="nama" id="" readonly>
-                    <input type="text" value="lorem adhiusgdwajhdn oaiwhd anp awd aowijda" class="pesan" id="" readonly>
-                </div>
-                <div class="waktu">
-                    <input type="text" value="19.00" class="jam" id="" readonly>
-                </div>
-            </div>
-        </a>
-    </div>
+
+    <?php       
+        }
+    ?>
 </body>
 </html>
